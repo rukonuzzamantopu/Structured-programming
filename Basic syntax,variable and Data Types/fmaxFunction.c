2f9bfdfe891119874a04b9abaaa2208ab86d617e
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-06 02:00:15 +0600

    c language can calculate max valu

---
+++ Basic syntax,variable and Data Types/fmaxFunction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  printf("%f\n", fmax(2.0, 0.25));
+  printf("%f\n", fmax(31.2, 18.0));
+  printf("%f\n", fmax(14, 22));
+  printf("%f\n", fmax(96, 2048));
+  return 0;
+}
