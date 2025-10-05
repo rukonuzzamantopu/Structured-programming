6433cf40242af15c1e281852b18da6ec002b103f
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-06 03:55:24 +0600

    add Practice Problem.txt in Arithmetic Operators

--- Arithmetic Operators/ifElse.c
+++ Arithmetic Operators/ifElse.c
@@ -4,10 +4,11 @@ int main(){
     int a;
     scanf("%d", &a);
     if(a>100){
-        printf("Greater than 100\n");
+        printf("Greater than 100");
     }
     else{
         printf("Less than or equal to 100\n");
     }
+    printf("topu");
     return 0;
 }
