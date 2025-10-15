fc0a11351a72cdee2ee70063adb2dfcdfd1f069b
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-16 00:16:13 +0600

    add https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/N slove

--- Problem solving with conditional statement/Char.c
+++ Problem solving with conditional statement/Char.c
@@ -1,12 +1,3 @@
-// Given a letter X. If the letter is lowercase print the letter after converting it from lowercase letter to uppercase letter. Otherwise print the letter after converting it from uppercase letter to lowercase letter
-
-// Note : difference between 'a' and 'A' in ASCII is 32 .
-
-// Input
-// Only one line containing a character X which will be a capital or small letter.
-
-// Output
-// Print the answer to this problem
 
 #include <stdio.h>
 int main(){
