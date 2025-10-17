d4e944e4a6618b11134aedf66582a1a9c0517bc2
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-17 16:13:43 +0600

    add assigment 2 solve

---
+++ Assignment 02/SumSum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d ", &arr[i]);
+    }
+    int pos=0, neg=0;
+   for(int i=0; i<n; i++){  
+    if(arr[i]>0){
+        pos += arr[i];
+    }else{
+        neg += arr[i];
+    }
+   }
+   printf("%d %d", pos, neg);
+  
+   return 0;
+}
+
