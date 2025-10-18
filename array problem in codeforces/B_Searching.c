fce96c3d2b656e6cad19de21d48c6310e17134cf
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-18 23:18:27 +0600

    add https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/B solve

---
+++ array problem in codeforces/B_Searching.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+   int target;
+   scanf("%d", &target);
+   for(int i=0; i<n; i++){
+        if(arr[i] == target){
+            printf("%d\n", i);
+            return 0;
+        }
+    }
+    printf("-1\n");
+    return 0;
+}
