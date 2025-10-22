32ce276b94d9b945d7b50ab85f3853addd1970df
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-22 23:52:13 +0600

    reverse array using for loop

--- Array/reverseAnArray.c
+++ Array/reverseAnArray.c
@@ -6,14 +6,13 @@ int main(){
     for(int i=0; i<n; i++){
         scanf("%d", &arr[i]);
     }
-    int i=0, j=n-1;
-    while(i<j){
+
+    for (int i=0, j=n-1; i<j; i++, j--) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-        i++;
-        j--;
     }
+
     for(int i=0; i<n; i++){
         printf("%d ", arr[i]);
     }
