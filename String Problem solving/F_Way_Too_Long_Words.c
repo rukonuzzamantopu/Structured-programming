e6402a8de3de7b70b02d9997cc0d0c43f9db4447
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-24 14:06:16 +0600

    add string copy oprtation

---
+++ String Problem solving/F_Way_Too_Long_Words.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    char a[100][1001];  
+
+    for (int i = 0; i < x; i++) {
+        scanf("%s", a[i]);  
+    }
+
+    for (int i = 0; i < x; i++) {
+        if()
+    }
+
+    return 0;
+}
