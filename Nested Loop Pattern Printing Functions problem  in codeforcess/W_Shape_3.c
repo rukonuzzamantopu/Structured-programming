3beb2ac52c9ff2db67cb999f8b8dc7aca4b42a95
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-11-06 23:18:24 +0600

    add pattern related codforces problem solve

---
+++ Nested Loop Pattern Printing Functions problem  in codeforcess/W_Shape_3.c
@@ -0,0 +1,25 @@
+#include <stdio.h> /
+
+void main()
+{
+   int i, j, r; 
+   scanf("%d", &r); 
+
+   for(i = 0; i <= r; i++) 
+   {
+     for(j = 1; j <= r - i; j++) 
+       printf(" ");
+     for(j = 1; j <= 2 * i - 1; j++) 
+       printf("*");
+     printf("\n");
+   }
+
+   for(i = r ; i >= 1; i--) 
+   {
+     for(j = 1; j <= r - i; j++) 
+       printf(" ");
+     for(j = 1; j <= 2 * i - 1; j++) 
+       printf("*");
+     printf("\n");
+   }
+}
