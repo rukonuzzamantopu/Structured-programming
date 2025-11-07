349a344b0db7d2804dfbdf3dbb159025e00d4d16
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-11-07 18:24:06 +0600

    complete assigment 3

--- Assigment 3/CountBeforeOne.c
+++ Assigment 3/CountBeforeOne.c
@@ -1,26 +1,27 @@
-
 #include <stdio.h>
 
-int count_before_one(int ar[], int l){
-    int c =  0;
-    for(int i = 0; i<l; i++){
-        if(ar[i] == 1){
-            return c;
+int count_before_one(int a[], int n) {
+    int c = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 1) {
+            break;  
         }
         c++;
     }
+    return c;
 }
 
 int main() {
     int n;
-    scanf("%d",&n);
-    int ar[n];
-    for(int i = 0; i<n; i++){
-        scanf("%d",&ar[i]);
+    scanf("%d", &n);
+    int a[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
 
-    int result = count_before_one(ar,n);
-    printf("%d", result);
+    int result = count_before_one(a, n);
+    printf("%d\n", result);
 
     return 0;
 }
