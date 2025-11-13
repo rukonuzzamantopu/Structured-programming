1b7285e6fc7e89065522cfcf4deaf7a7e8e6b60e
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-11-14 03:52:51 +0600

    i want to die

--- Final/Find_the_Missing_Number.c
+++ Final/Find_the_Missing_Number.c
@@ -1,34 +1,81 @@
+// Problem Statement
+
+// Given the multiplication of four numbers and three of those numbers, find the missing number.
+
+// Note: If the missing number could not be found for the given input, print -1. All numbers are non-negative integers.
+
+// Input Format
+
+// The first line will contain T, number of test cases.
+// For every test case, the input will contain one integer M (the multiplication of the four numbers), A, B, C (three of those four numbers).
+// Constraints
+
+// 1 < T <= 100000
+// 0 <= M <= 10^18
+// 1 <= A, B, C <= 10^6
+// Output Format
+
+// Print the missing number. Don't forget to print a new line after it.
+// Sample Input 0
+
+// 4
+// 20 1 2 2
+// 10 2 2 1
+// 1 1 2 3
+// 0 3 10 15
+// Sample Output 0
+
+// 5
+// -1
+// -1
+// 0
+// Sample Input 1
+
+// 3
+// 20 5 1 1
+// 18 2 3 3
+// 0 10 20 30
+// Sample Output 1
+
+// 4
+// 1
+// 0
+// Submissions: 224
+// Max Score: 20
+// Difficulty: Easy
+// Rate This Challenge:
+
+// More
 
 #include <stdio.h>
-
 int main()
 {
     int t;
     scanf("%d", &t);
-
-    for (int i = 0; i < t; i++)
+    while (t--)
     {
-        long long int m;
-        int a, b, c;
-        scanf("%lld %d %d %d", &m, &a, &b, &c);
-        int mul = a * b * c;
-
-        if (m == 0)
+        long long m, a, b, c;
+        scanf("%lld %lld %lld %lld", &m, &a, &b, &c);
+        if (a == 0 || b == 0 || c == 0)
         {
-            printf("0");
-            continue;
-        }
-        for (int j = 1; j <= m; j++)
-        {
-            if (mul * j == m)
+            if (m == 0)
             {
-                printf("%d\n", j);
-                break;
+                printf("0\n");
             }
-            else if (mul * j > m)
+            else
             {
                 printf("-1\n");
-                break;
+            }
+        }
+        else
+        {
+            if (m % (a * b * c) == 0)
+            {
+                printf("%lld\n", m / (a * b * c));
+            }
+            else
+            {
+                printf("-1\n");
             }
         }
     }
