76205060a84b0cc42f565e0eb5b3f12d70ef46e3
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-11-14 02:33:17 +0600

    add phitron final exam qution solve

---
+++ Final/Find_the_Missing_Number.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        long long int m;
+        int a, b, c;
+        scanf("%d %d %d %d", &m, &a, &b, &c);
+        int mul = a * b * c;
+
+        if (m == 0)
+        {
+            printf("0");
+            continue;
+        }
+        for (int j = 1; j <= m; j++)
+        {
+            if (mul * j == m)
+            {
+                printf("%d\n", j);
+                break;
+            }
+            else if (mul * j > m)
+            {
+                printf("-1\n");
+                break;
+            }
+        }
+    }
+    return 0;
+}
