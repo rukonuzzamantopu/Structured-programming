d4d17e755a2419d5d5c03fca11e079bc587fe9d9
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-11-14 04:11:26 +0600

    solve find the missing number problem

--- Final/Find_the_Missing_Number.c
+++ Final/Find_the_Missing_Number.c
@@ -1,52 +1,3 @@
-// Problem Statement
-
-// Given the multiplication of four numbers and three of those numbers, find the missing number.
-
-// Note: If the missing number could not be found for the given input, print -1. All numbers are non-negative integers.
-
-// Input Format
-
-// The first line will contain T, number of test cases.
-// For every test case, the input will contain one integer M (the multiplication of the four numbers), A, B, C (three of those four numbers).
-// Constraints
-
-// 1 < T <= 100000
-// 0 <= M <= 10^18
-// 1 <= A, B, C <= 10^6
-// Output Format
-
-// Print the missing number. Don't forget to print a new line after it.
-// Sample Input 0
-
-// 4
-// 20 1 2 2
-// 10 2 2 1
-// 1 1 2 3
-// 0 3 10 15
-// Sample Output 0
-
-// 5
-// -1
-// -1
-// 0
-// Sample Input 1
-
-// 3
-// 20 5 1 1
-// 18 2 3 3
-// 0 10 20 30
-// Sample Output 1
-
-// 4
-// 1
-// 0
-// Submissions: 224
-// Max Score: 20
-// Difficulty: Easy
-// Rate This Challenge:
-
-// More
-
 #include <stdio.h>
 int main()
 {
@@ -56,27 +7,14 @@ int main()
     {
         long long m, a, b, c;
         scanf("%lld %lld %lld %lld", &m, &a, &b, &c);
-        if (a == 0 || b == 0 || c == 0)
+
+        if (m % (a * b * c) == 0)
         {
-            if (m == 0)
-            {
-                printf("0\n");
-            }
-            else
-            {
-                printf("-1\n");
-            }
+            printf("%lld\n", m / (a * b * c));
         }
         else
         {
-            if (m % (a * b * c) == 0)
-            {
-                printf("%lld\n", m / (a * b * c));
-            }
-            else
-            {
-                printf("-1\n");
-            }
+            printf("-1\n");
         }
     }
     return 0;
