03c2da42d3928c93e6b2a6e9b2067a2818e5eadd
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2026-01-13 17:35:07 +0600

    revise 2D Array

---
+++ Array/2D Array/checkingSquareMatrix.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int arr[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    if (r == c)
+    {
+        printf("Square Matrix\n");
+    }
+    else
+    {
+        printf("not square  Matrix\n");
+    }
+    return 0;
+}
